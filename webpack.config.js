2ad7e7d29adc7ac40a7da19bb23d07672dc52133
iemwill <33894930+iemwill@users.noreply.github.com>
2020-04-28 16:41:44 +0200

    remove error-causing unnecessary module-section

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,17 @@ module.exports = {
     entry: "./rsk-conversion-utils.js",
     output: {
         path: __dirname + '/build/lib',
-		filename: 'rsk-conversion-utils.js',
-		libraryTarget: 'var',
-		library: 'RSKUtils'
+	filename: 'rsk-conversion-utils.js',
+	libraryTarget: 'var',
+	library: 'RSKUtils'
     },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-			{ test: /\.json$/, loader: "json-loader" }
-        ]
-    },
-	plugins: [
+    plugins: [
         new CopyWebpackPlugin([
             { from: './index.html', to: '../index.html' },
-			{ from: './lib/jquery-3.1.1.min.js' },
-			{ from: './rsk-helper.js' },
-			{ from: './css', to: '../css'},
+	    { from: './lib/jquery-3.1.1.min.js' },
+	    { from: './rsk-helper.js' },
+	    { from: './css', to: '../css'},
             { from: './img', to: '../img'}
-		])
-	]
+	])
+    ]
 };
